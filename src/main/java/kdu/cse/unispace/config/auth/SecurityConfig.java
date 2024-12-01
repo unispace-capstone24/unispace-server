0ef709e97f3b7ebcb188e8e1135c906b22022adf
unispace-capstone24 <unispacecs@gmail.com>
2024-12-01 14:09:32 +0900

    Update SecurityConfig.java
    Signed-off-by: unispace-capstone24 <unispacecs@gmail.com>

--- src/main/java/kdu/cse/unispace/config/auth/SecurityConfig.java
+++ src/main/java/kdu/cse/unispace/config/auth/SecurityConfig.java
@@ -1,32 +1,27 @@
 package kdu.cse.unispace.config.auth;
 
-import jakarta.servlet.http.HttpServletResponse;
-import kdu.cse.unispace.config.jwt.JwtAuthenticationFilter;
-import kdu.cse.unispace.config.jwt.JwtAuthenticationSuccessHandler;
-import kdu.cse.unispace.domain.Member;
 import kdu.cse.unispace.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity // Spring Security 설정 활성화
 @RequiredArgsConstructor
 //@EnableGlobalAuthentication
-@EnableGlobalMethodSecurity(prePostEnabled = true) //@PreAuthorize @PostAuthorize 활성화
 public class SecurityConfig {
 
 
@@ -40,102 +35,50 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    private final JwtAuthenticationSuccessHandler jwtAuthenticationSuccessHandler;
-
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
     private final MemberService memberService;
 
 
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-//        configuration.setAllowedHeaders(Arrays.asList("*"));
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()  //csrf disable
-                //.cors().disable()
-                //.cors().and()
-
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(requests -> requests //로그인 전
-                        //.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/", "/home", "/signup", "/member", "/login","/oauth2/**", "/profile").permitAll()
-                        .requestMatchers("/ws/**", "/sub/**", "/pub/**").permitAll()
-
-                        //.anyRequest().permitAll() //임시로 모든 페이지 접근 허용
-
-                        .anyRequest().authenticated() // 어떠한 요청이라도 인증이 필요
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/home", "/signup", "/member", "/login", "/profile").permitAll() // 로그인 전 접근 가능
+                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
-
-
-                //일반 로그인
-                .formLogin((form) -> form // form 방식 로그인 사용
-                        .loginPage("/login")  //로그인 페이지
-                        .loginProcessingUrl("/login-process") //submit을 받을 url - 시큐리티가 처리해줌(MyMemberDetailService로 넘겨준것을)
-                        .usernameParameter("email") //submit할 아이디(이메일)
-                        .passwordParameter("password") //submit할 비밀번호
-                        //.defaultSuccessUrl("/login", true) //성공시
-                        .failureUrl("/login") //로그인 실패시 다시 로그인화면
-                        .permitAll()  // 로그인 페이지 이동이 막히면 안되므로 관련된애들 모두 허용
-                        .successHandler(jwtAuthenticationSuccessHandler)
+                // 일반 로그인 설정
+                .formLogin(form -> form
+                        .loginPage("/login")  // 로그인 페이지 경로
+                        .loginProcessingUrl("/login-process") // 로그인 폼 제출 URL
+                        .usernameParameter("email") // 이메일 필드 이름
+                        .passwordParameter("password") // 비밀번호 필드 이름
+                        .defaultSuccessUrl("/home", true) // 로그인 성공 시 이동할 페이지
+                        .failureUrl("/login?error=true") // 로그인 실패 시 이동할 페이지
+                        .permitAll() // 로그인 관련 URL은 인증 없이 접근 가능
                 )
+                // 로그아웃 설정
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // 로그아웃 URL
+                        .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 URL
+                        .invalidateHttpSession(true) // 세션 무효화
+                        .deleteCookies("JSESSIONID") // 쿠키 삭제
+                );
 
-
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/oauth2/authorization/google")
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(googleOauth2UserService)
-                        )
-                        .defaultSuccessUrl("/main", true) //성공시
-                        .permitAll()
-                )
-
-
-                .logout() // 로그아웃시 /로 이동
-
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    if (authentication != null && authentication.getDetails() != null) {
-                        try {
-                            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-                            Member member = memberService.findOne(customUserDetails.getId());
-                            member.changeUuid(UUID.randomUUID()); // change UUID to invalidate all tokens
-                            memberService.save(member); // persist the updated user record
-                            request.getSession().invalidate(); // invalidate session
-                            response.getWriter().write("로그아웃 완료");
-                            response.setStatus(HttpServletResponse.SC_OK);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                })
-
-//                    .logoutSuccessHandler((request, response, authentication) -> {
-//                        response.getWriter().write("로그아웃 완료");
-//                        response.setStatus(HttpServletResponse.SC_OK);
-//                    });
-
-
-        //.logout(logout->logout.logoutSuccessUrl("/")) // 로그아웃시 /로 이동
-
-
-        //.httpBasic()//postman 사용시 필요
-
-        ;
         return http.build();
     }
-
 }
