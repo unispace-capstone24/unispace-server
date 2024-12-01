f38cf0e0afedb03666bec2235570c32ca8047aea
unispace-capstone24 <unispacecs@gmail.com>
2024-12-01 14:07:59 +0900

    JWT 미사용으로 인한 주석처리
    Signed-off-by: unispace-capstone24 <unispacecs@gmail.com>

--- src/main/java/kdu/cse/unispace/config/jwt/JwtTokenExtractor.java
+++ src/main/java/kdu/cse/unispace/config/jwt/JwtTokenExtractor.java
@@ -1,17 +1,17 @@
-package kdu.cse.unispace.config.jwt;
+// package kdu.cse.unispace.config.jwt;
 
-import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletRequest;
 
-public class JwtTokenExtractor {  
-    public static final String HEADER_PREFIX = "Bearer ";
+// public class JwtTokenExtractor {  
+//     public static final String HEADER_PREFIX = "Bearer ";
 
-    public static String extract(HttpServletRequest request) {
-        String header = request.getHeader("JWT-Authorization");
+//     public static String extract(HttpServletRequest request) {
+//         String header = request.getHeader("JWT-Authorization");
 
-        if (header == null || header.isEmpty() || !header.startsWith(HEADER_PREFIX)) {
-            return null;
-        }
+//         if (header == null || header.isEmpty() || !header.startsWith(HEADER_PREFIX)) {
+//             return null;
+//         }
 
-        return header.substring(HEADER_PREFIX.length());
-    }
-}
+//         return header.substring(HEADER_PREFIX.length());
+//     }
+// }
