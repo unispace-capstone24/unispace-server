da37c12a1fd989b95f371e8325f050443025d1c8
unispace-capstone24 <unispacecs@gmail.com>
2024-12-01 14:06:56 +0900

    JWT 미
    Signed-off-by: unispace-capstone24 <unispacecs@gmail.com>

--- src/main/java/kdu/cse/unispace/config/jwt/JwtAuthenticationSuccessHandler.java
+++ src/main/java/kdu/cse/unispace/config/jwt/JwtAuthenticationSuccessHandler.java
@@ -1,50 +1,50 @@
-package kdu.cse.unispace.config.jwt;
+// package kdu.cse.unispace.config.jwt;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import kdu.cse.unispace.domain.Member;
-import kdu.cse.unispace.service.MemberService;
-import net.minidev.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import kdu.cse.unispace.domain.Member;
+// import kdu.cse.unispace.service.MemberService;
+// import net.minidev.json.JSONObject;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+// import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+// import java.io.IOException;
+// import java.io.PrintWriter;
 
 
-@Component
-public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+// @Component
+// public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
+//     @Autowired
+//     private JwtTokenUtil jwtTokenUtil;
 
-    @Autowired
-    private MemberService memberService;
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+//     @Autowired
+//     private MemberService memberService;
+//     @Override
+//     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+//                                         Authentication authentication) throws IOException, ServletException {
 
-        boolean rememberMe = "true".equals(request.getParameter("remember-me"));
+//         boolean rememberMe = "true".equals(request.getParameter("remember-me"));
 
-        String email = authentication.getName();
-        Member member = memberService.findByEmail(email);
+//         String email = authentication.getName();
+//         Member member = memberService.findByEmail(email);
 
-        // 토큰 생성
-        String token = jwtTokenUtil.generateToken(authentication, rememberMe);
+//         // 토큰 생성
+//         String token = jwtTokenUtil.generateToken(authentication, rememberMe);
 
-        // JSON 형식으로 응답 바디에 토큰 추가
-        JSONObject json = new JSONObject();
-        json.put("token", token);
-        json.put("id", member.getId());
+//         // JSON 형식으로 응답 바디에 토큰 추가
+//         JSONObject json = new JSONObject();
+//         json.put("token", token);
+//         json.put("id", member.getId());
 
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter writer = response.getWriter();
-        writer.write(json.toString());
-        writer.flush();
+//         response.setContentType("application/json");
+//         response.setCharacterEncoding("UTF-8");
+//         PrintWriter writer = response.getWriter();
+//         writer.write(json.toString());
+//         writer.flush();
 
-    }
-}
+//     }
+// }
