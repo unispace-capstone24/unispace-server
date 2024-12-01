c0ca9ccef0264fa85f8aea723920116aa8fea0fe
unispace-capstone24 <unispacecs@gmail.com>
2024-12-01 14:21:27 +0900

    Update CustomHandshakeInterceptor.java
    Signed-off-by: unispace-capstone24 <unispacecs@gmail.com>

--- src/main/java/kdu/cse/unispace/config/websocket/CustomHandshakeInterceptor.java
+++ src/main/java/kdu/cse/unispace/config/websocket/CustomHandshakeInterceptor.java
@@ -1,22 +1,13 @@
 package kdu.cse.unispace.config.websocket;
 
-import kdu.cse.unispace.config.jwt.JwtAuthenticationFilter;
-import kdu.cse.unispace.config.jwt.JwtTokenUtil;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
@@ -24,21 +15,19 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CustomHandshakeInterceptor implements HandshakeInterceptor {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final JwtTokenUtil tokenUtil;
-    final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
                                    WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-
-        log.info("beforeHandshake가 실행 - WebSocket 연결의 핸드셰이크 단계에서 추가 동작을 수행하는 역할 (아직 뭔가를 해주진 않음)");
-
-        return true;
+        log.info("WebSocket 연결 요청이 감지되었습니다.");
+        // 필요한 경우 요청 헤더나 URL에서 추가 데이터를 확인 가능
+        return true; // 연결 허용
     }
 
     @Override
     public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
                                WebSocketHandler wsHandler, Exception exception) {
+        log.info("WebSocket 연결이 완료되었습니다.");
     }
 }
