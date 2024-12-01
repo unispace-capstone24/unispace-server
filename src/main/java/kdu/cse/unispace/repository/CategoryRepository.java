af622cfce717c2440b0b4f8e3467495643d3eb97
unispace-capstone24 <unispacecs@gmail.com>
2024-12-01 14:47:44 +0900

    Update CategoryRepository.java
    Signed-off-by: unispace-capstone24 <unispacecs@gmail.com>

--- src/main/java/kdu/cse/unispace/repository/CategoryRepository.java
+++ src/main/java/kdu/cse/unispace/repository/CategoryRepository.java
@@ -3,5 +3,5 @@ package kdu.cse.unispace.repository;
 import kdu.cse.unispace.domain.space.schedule.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository<Category> extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
